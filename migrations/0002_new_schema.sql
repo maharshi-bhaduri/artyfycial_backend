e7805b417d36113555189d568d0d8dae0f32fa17
xav1994das <xav0anime@gmail.com>
2024-06-09 16:45:49 +0530

    changes in package and package lock

---
+++ migrations/0002_new_schema.sql
@@ -0,0 +1,49 @@
+-- Migration number: 0002 	 2024-06-08T19:27:18.175Z
+
+-- Migration number: 0002 	 2024-06-08T19:24:32.853Z
+
+-- Migration number: 0001 	 2024-06-07T19:09:08.074Z
+
+-- Drop table if exists statements
+DROP TABLE IF EXISTS exhibition;
+DROP TABLE IF EXISTS artwork;
+DROP TABLE IF EXISTS artist;
+
+-- Table: artist
+CREATE TABLE artist (
+    artistId INTEGER PRIMARY KEY AUTOINCREMENT,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    about TEXT,
+    socials VARCHAR(255),
+    phoneNumber VARCHAR(20),
+    location VARCHAR(255),
+    joinDate DATE NOT NULL,
+    active BOOLEAN NOT NULL
+);
+
+-- Table: artwork
+CREATE TABLE artwork (
+    artworkId INTEGER PRIMARY KEY AUTOINCREMENT,
+    artistId INTEGER,
+    title VARCHAR(255) NOT NULL,
+    date DATE,
+    description TEXT,
+    active BOOLEAN NOT NULL,
+    path VARCHAR(255),
+    FOREIGN KEY (artistId) REFERENCES artist(artistId)
+);
+
+-- Table: exhibition
+CREATE TABLE exhibition (
+    exhibitionId INTEGER PRIMARY KEY AUTOINCREMENT,
+    artistId INTEGER,
+    artworkId INTEGER,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    startDate DATE NOT NULL,
+    endDate DATE NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    FOREIGN KEY (artistId) REFERENCES artist(artistId),
+    FOREIGN KEY (artworkId) REFERENCES artwork(artworkId)
+);
