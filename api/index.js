68bb22b131f96587b5ca5e4fcb230da9796f9ece
Maharshi Bhaduri <bhaduri.maharshi@gmail.com>
2024-06-23 23:01:37 +0530

    updated data model and add image api

--- api/index.js
+++ api/index.js
@@ -1,14 +1,14 @@
 import Router from "router";
 import finalhandler from "finalhandler";
 const getTime = require("./getTime");
-const createImage = require("./createImage");
+const addImage = require("./addImage");
 const getGallery = require("./getGallery");
 const getFeed = require("./getFeed");
 import { allowCors } from "../utils/utils";
 
 const router = Router();
 router.get("/api/getTime", getTime);
-router.post("/api/createImage", createImage);
+router.post("/api/addImage", addImage);
 router.get("/api/getGallery", getGallery);
 router.get("/api/getFeed", getFeed);
 
